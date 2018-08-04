853ae3f6424583bb9170387c61f4e77b8e2b991e
arblalock <aadamrblalock@gmail.com>
2018-08-03 17:25:00 -0700

    added initial route

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.get('/', function (req, res) {
 })
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({greeting: 'hello API'})
+app.get('/api/whoami', function (req, res) {
+  res.json({ipaddress: req.headers['x-forwarded-for'], language: req.headers['accept-language'], 'software': req.get('User-Agent')})
 })
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port)
 })
